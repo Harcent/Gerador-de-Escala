d4f371dccd7d9ad7afbaec146287e9c1508bbde9
HelioS <helioarcanet@gmail.com>
2023-12-15 13:20:40 -0300

    Add files via upload

--- escala.py
+++ escala.py
@@ -29,7 +29,7 @@ class Escala:
     pessoas = self.pessoas
     while len(pessoas) > 0:
       desejos = self.desejos
-      for pessoa in pessoas:
+      for pessoa in pessoas.copy():
         for desejo in desejos[pessoa].copy(): 
           if desejo in self.vacancias:
             if self.escala[pessoa]:
@@ -42,13 +42,13 @@ class Escala:
               self.escala[pessoa][desejo] = desejos[pessoa].pop(desejo)
               break
           desejos[pessoa].pop(desejo)
-        if len(desejos[pessoa]) == 0:
+        if len(desejos[pessoa]) == 0 and type(self.escala[pessoa]) != str:
           final = sorted(self.escala[pessoa].keys(), key=lambda x: (int(x[:-1]), x[-1]))
           count = len(final)
           plural_suffix = 'ões' if count != 1 else 'ão'
           final = f'{" ".join(final)} ({count} plant{plural_suffix})'
           self.escala[pessoa] = final
-          pessoas.remove(pessoa)
+          self.pessoas.remove(pessoa)
 
   def conflito(self, escala, novoPlantao, pessoa):
     """
@@ -105,7 +105,7 @@ class Escala:
     v = len(self.vacancias)
     print(f"Vacância{'s' if v != 1 else ''}: {' '.join(self.vacancias)} ({v} plant{'ões' if v != 1 else 'ão'})" if v else "Não houve vacâncias")
     
-prioridade = "Guilherme Vital Heltron Ribeiro Ramon Gleide Moisés Amanda_G Luiza Kariny Délio Amanda_A".split(" ")
+prioridade = "Guilherme Vital Heltron Ribeiro Ramon Gleide Moisés Amanda_G Luiza Kariny Délio Amanda_V".split(" ")
 desejos = {}
 desejos[0] = "2D 9D 23D 30D 4D 11D 18D 4N 11N 18N 2N 9N 23N 30N 5D 26D 12N 19N 26N 8D 8N 15D 22D 29D 29N 20N 27N 7D 14D 28D 7N 14N 21N 28N 6D 6N 25D 25N 16D 16N" #Guilherme
 desejos[1] = "8N 9D 18N 22N 23N 24N 25N 29N 19D 19N 20D 20N 21N 22D 24D 25D 26D 28N 29D 9N 16N 26N 30N" #Vital
@@ -119,6 +119,16 @@ desejos[8] = "5D 7N 8D 9D 10D" #Luiza
 desejos[9] = "17N 12D 26D 14N 18D 3D 10D 24D 31D 20N 6N" #Kariny
 desejos[10] = "4D 11D 4N 11N 3N 10N 12N 6D" #Délio
 desejos[11] = "9D 23D 9N 23N 16D 10D 17D 24D 31D 19D 10N 17N 24N 31N 14N 21N 28N" #Amanda V
+
+for d in desejos:
+  temp = desejos[d].split(" ")
+  temp = [i for i in temp if i != '']
+  for idx, item in enumerate(temp):
+    dia, turno = int(item[:-1]), item[-1]
+    item = f'{dia}{turno}'
+    temp[idx] = item
+  
+  desejos[d] = " ".join(temp)
 
 spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTEYffAo-dy94auNHe4OjgUpyg9MvTc20iUCqjv68l61umYUHbBoqgAfFOf6uN9SGogvImLz-YE3_h1/pub?gid=0&single=true&output=csv"
 df = pd.read_csv(spreadsheet_url)
